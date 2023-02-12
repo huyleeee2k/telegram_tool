5afaae61636c07b8c7568f5a384c89c61cb5f50e
huy_leeee <63085332+huyleeee2k@users.noreply.github.com>
2023-02-12 21:14:03 +0700

    update connect api

--- application_telegram/connectAPI.py
+++ application_telegram/connectAPI.py
@@ -2,9 +2,9 @@ from telethon.sync import TelegramClient
 #Kết nối với telegram
 def connectClientTelegram():
     try:
-        api_id = 18812462
-        api_hash = "d9ee8054d89b7020eb90f064c1acae6a"
-        phone = "+84 354482801"
+        api_id = your_api_id
+        api_hash = your_api_hash
+        phone = your_phone
         client = TelegramClient(phone,api_id,api_hash)
         return True
     except KeyError:
